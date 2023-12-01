9612220e8a90a0dfe46ecdb068785e551979bf73
LuizFC-Testes <luiz_entt@outlook.com>
2023-12-01 14:22:40 -0300

    Revert "fabfile.py alterado para receber parâmetros"
    This reverts commit 8d70df44238fa7cc50ad71f776c076d8002db181.

--- fabfile.py
+++ fabfile.py
@@ -9,11 +9,9 @@ from src import model_predict
 from src import model_train
 
 @task
-def TrainModel(dataset_folder_path=None, output_model_path=None):
-    if dataset_folder_path == None:
-        dataset_folder_path = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/Datasets/TrashDetectionDataset"
-    if output_model_path == None:
-        output_model_path   = "runs" 
+def TrainModel(c):
+    dataset_folder_path = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/Datasets/TrashDetectionDataset"
+    output_model_path   = "runs" 
 
     dataset_folder_path = cv.read_folder_path(dataset_folder_path)
     output_model_path   = cv.read_folder_path(output_model_path)
@@ -21,13 +19,10 @@ def TrainModel(dataset_folder_path=None, output_model_path=None):
     model_train.run_training(dataset_folder_path, output_model_path) 
     
 @task
-def PredictImages(images_folder_path=None, output_folder_path=None, model_path=None):
-    if images_folder_path == None:
-        images_folder_path = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/Datasets/TrashDetectionDataset/test/images"
-    if output_folder_path == None:
-        output_folder_path = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/results/test1" 
-    if model_path == None:
-        model_path         = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/runs/segment/train5/weights/best.pt"
+def PredictImages(c):
+    images_folder_path = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/Datasets/TrashDetectionDataset/test/images"
+    output_folder_path = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/results/test1" 
+    model_path         = "/home/lucas/Documentos/Universidade/Disciplinas/MC886/ProjetoFinal/TrashDetection/runs/segment/train5/weights/best.pt"
 
     images_folder_path = cv.read_folder_path(images_folder_path)
     output_folder_path = cv.read_folder_path(output_folder_path)
